aa427c6120fd7a06682feb9aa328e9ec40b6f5ca
Tom Dyer <tdyer@ourstage.com>
2014-05-08 00:22:24 -0400

    Update the product seeds to have a category

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140505214900) do
+ActiveRecord::Schema.define(version: 20140508040315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20140505214900) do
     t.float    "price"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "category"
   end
 
   create_table "songs", force: true do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,16 @@
+puts 'Deleting all products'
 Product.delete_all
-Product.create(name: "Brown Shoe", description: "It's brown, it's a shoe", price: 13.99)
-Product.create(name: "Rose Colored Glasses", description: "To make you happy, happy", price: 23.49)
-Product.create(name: 'Motorized Fork', description: 'To stuff your gob more effienctly', price: 12.07)
-Product.create(name: 'Brown Paper Bag', description: "Hide your booze, openly, in public for everyone to see", price: 1.44)
-Product.create(name: 'Bouncy Castle', description: 'Perfect software development environment',  price: 1_200.00)
-Product.create(name: 'Headphones', description: 'Phone for you head', price: 23.66)
+puts 'Creating products'
+Product.create(name: "Brown Shoe", description: "It's brown, it's a shoe", price: 13.99, category: 'Apparel')
+Product.create(name: "Rose Colored Glasses", description: "To make you happy, happy", price: 23.49, category: 'Apparel')
+Product.create(name: 'Motorized Fork', description: 'To stuff your gob more effienctly', price: 12.07, category: 'Kitchen')
+Product.create(name: 'Brown Paper Bag', description: "Hide your booze, openly, in public for everyone to see", price: 1.44, category: 'Kitchen')
+Product.create(name: 'Bouncy Castle', description: 'Perfect software development environment',  price: 1_200.00, category: 'Entertainment')
+Product.create(name: 'Headphones', description: 'Phone for you head', price: 23.66, category: 'Entertainment')
 
+puts 'Deleting all products'
 Song.delete_all
+puts 'Creating songs'
 Song.create(title: 'Golden Age', genre: 'Rock', price: 1.49)
 Song.create(title: 'Breathe', genre: 'ProgRock', price: 2.49)
 Song.create(title: 'Norwegian Wood', genre: 'Rock', price: 1.99)
