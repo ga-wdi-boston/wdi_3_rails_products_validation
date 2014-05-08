b6d41714dd3fbf9bc694130b30c40ca35270b55f
Tom Dyer <tdyer@ourstage.com>
2014-05-08 00:24:36 -0400

    Added the product category to the strong params

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -64,6 +64,6 @@ class ProductsController < ApplicationController
   end
 
   def product_params
-    params.require(:product).permit([:name, :description, :price])
+    params.require(:product).permit([:name, :description, :price, :category])
   end
 end
