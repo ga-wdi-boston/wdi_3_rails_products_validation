fb4ab5394ff3a6a2fafc25f09461e6edea9e58c8
Tom Dyer <tdyer@ourstage.com>
2014-05-08 01:21:23 -0400

    Added product validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,18 @@
 class Product < ActiveRecord::Base
+
+  CATEGORIES = ['Apparel', 'Kitchen', 'Entertainment', 'Software']
+  
   # A Product MUST have a name
   validates :name, presence: true
+  # A Product name MUST be at least 3 characters.
+  validates :name, length: { minimum: 3}
+  # A Product name MUST be unique
+  validates :name, uniqueness: true
+  # The price MUST be a number
+  # validates :price, numericality: true
+  # The price MUST be of the format that 'matches' the regular expression, regex.
+  validates :price, format: { :with => /\A\d+(\.\d{2})?\z/, message: 'Must be a zero or more digits, a period, followed by two digits' }
+  # A category must be one the above
+  validates :category, inclusion: { in: CATEGORIES }
   
 end
